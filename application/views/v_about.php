9e74b44b14d86c647655f7512f57540d07bcc5c2
Danuar <donidanuar86@gmail.com>
2024-04-22 15:53:46 +0700

    Tugas_3

---
+++ application/views/v_about.php
@@ -0,0 +1,21 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Doni</li>
+        <li>Nama Belakang : Danuar</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Rasau Jaya 3, Jl.Margodadi</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Kec.Sungai Raya, Desa Kalibandung</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Game</li>
+    </ul>
+</section>
