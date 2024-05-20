7ba77f858ecb11b500a40a35509e606584c6e4a8
Danuar <donidanuar86@gmail.com>
2024-05-20 15:29:00 +0700

    Tugas 7 Pertemuan 9

--- application/models/Model_dlemas.php
+++ application/models/Model_dlemas.php
@@ -1,17 +1,17 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Dlemas_model extends CI_Model
+class Model_dlemas extends CI_Model
 {
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
     public function simpan_data($data)
     {
-        // Proses penyimpanan data siswa ke dalam database
-        return $this->db->insert('view_data_dlemas', $data);
+        // Simpan data siswa ke dalam database
+        $this->db->insert('view_form_dlemas', $data);
+        
+        // Anda bisa tambahkan kode lain di sini, misalnya penanganan error atau notifikasi
+        
+        return true; // Berhasil menyimpan data
     }
+
+    // Anda dapat menambahkan metode lain di sini sesuai kebutuhan, misalnya untuk mengambil data siswa dari database
 }
